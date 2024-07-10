0aa683a05286f44d41cd7419645fde69658a2aa1
Taylor Monacelli <taylormonacelli@gmail.com>
2024-07-10 16:04:35 -0700

    failing

--- cmd/hello.go
+++ cmd/hello.go
@@ -11,11 +11,14 @@ var helloCmd = &cobra.Command{
 	Long:  `A longer description that spans multiple lines and likely contains examples and usage of using your command.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		logger := LoggerFrom(cmd.Context())
+		logger.V(1).Info("Debug: Entering hello command Run function")
 		logger.Info("Running hello command")
 		core.Hello(logger)
+		logger.V(1).Info("Debug: Exiting hello command Run function")
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(helloCmd)
+	logger.V(1).Info("Debug: Added hello command to root command")
 }
--- core/core.go
+++ core/core.go
@@ -1,13 +1,11 @@
 package core
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"strings"
 
 	"github.com/go-logr/logr"
-
 	"golang.org/x/net/html"
 )
 
@@ -17,33 +15,49 @@ func Hello(logger logr.Logger) {
 	logger.V(1).Info("Debug: Exiting Hello function")
 
 	url := "https://open.substack.com/pub/systemdesignone/p/saga-design-pattern?utm_source=share&utm_medium=android&r=21036"
-	title, err := getPageTitle(url)
+	logger.V(1).Info("Debug: About to fetch page title", "url", url)
+	title, err := getPageTitle(logger, url)
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+		logger.Error(err, "Failed to get page title")
 		return
 	}
-	fmt.Printf("Page title: %s\n", title)
+	logger.Info("Fetched page title", "title", title)
 }
 
-func getPageTitle(url string) (string, error) {
+func getPageTitle(logger logr.Logger, url string) (string, error) {
+	logger.V(1).Info("Debug: Entering getPageTitle function", "url", url)
+
 	resp, err := http.Get(url)
 	if err != nil {
+		logger.Error(err, "Failed to make HTTP request")
 		return "", err
 	}
 	defer resp.Body.Close()
 
+	logger.V(1).Info("Debug: HTTP request successful", "status", resp.Status)
+
 	tokenizer := html.NewTokenizer(resp.Body)
+	logger.V(1).Info("Debug: Created HTML tokenizer")
+
 	for {
 		tokenType := tokenizer.Next()
+		logger.V(2).Info("Debug: Processing token", "type", tokenType)
+
 		switch tokenType {
 		case html.ErrorToken:
+			logger.V(1).Info("Debug: Reached end of HTML document")
 			return "", io.EOF
 		case html.StartTagToken, html.SelfClosingTagToken:
 			token := tokenizer.Token()
+			logger.V(2).Info("Debug: Found start/self-closing tag", "tag", token.Data)
+
 			if token.Data == "title" {
+				logger.V(1).Info("Debug: Found title tag")
 				tokenType = tokenizer.Next()
 				if tokenType == html.TextToken {
-					return strings.TrimSpace(tokenizer.Token().Data), nil
+					title := strings.TrimSpace(tokenizer.Token().Data)
+					logger.V(1).Info("Debug: Extracted title", "title", title)
+					return title, nil
 				}
 			}
 		}
